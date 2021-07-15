06e5d35c5dd12dc29a4c24cba9498a166d1a9553
amir <amir15bfk@gmail.com>
2021-07-15 11:31:33 +0100

    deleting blooks + go up and down + exit cursor

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
+import sys
+def update():
+    if held_keys['k']:
+        mouse.locked = False
+    if held_keys['l']:
+        mouse.locked = True
+    if held_keys['r']:
+        player.y+=10*time.dt
+    if held_keys['f']:
+        player.y-=10*time.dt
+
 class Voxel(Button):
     def __init__(self,position=(0,0,0)):
         super().__init__(
@@ -8,20 +19,22 @@ class Voxel(Button):
             model = 'cube',
             origin_y = 0.5,
             texture ='white_cube',
-            color = color.white,
+            color = color.color(0,0,random.uniform(0.9,1)),
             highlight_color = color.lime
         )
     def input(self,key):
         if self.hovered:
             if key == 'left mouse down':
                 voxel =  Voxel(position = self.position + mouse.normal)
-
+            if key == 'right mouse down':
+                destroy(self)
+            
 app = Ursina()
 
 for z in range(-8,8):
     for x in range(-8,8):
-        voxel = Voxel((x,-2,z))
+        voxel = Voxel((x,-10,z))
 player = FirstPersonController()
-
+player.gravity=0
 
 app.run()
